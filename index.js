b2e43f087a93e94b0dc0f2d5b9a982fefc9d1d60
Matthew-Tangk <matthew.tangkilissan@gmail.com>
2023-04-25 18:33:54 +0200

    backend les 2 update

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello world')
+console.log('This is a testrun.')
+
+const express = require('express')
+
+express()
+    .get('/', onhome)
+    .get('/about', onabout)
+    .get('/test', onatest)
+
+    .use(express.static('static'))
+    .set('view engine', 'ejs')
+    .set('views', 'views')
+
+
+    .listen(420)
+
+    function onhome(req, res) {
+        res.send('<h1>This is a testrun.</h1>')
+    }
+
+    function onabout(req, res) {
+        res.send('<h1>About me</h1>')
+    }
+
+    function onatest(req, res){
+        res.render ('test.ejs') 
+    }
