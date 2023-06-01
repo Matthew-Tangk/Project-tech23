9bb1ccbf24f0d305c92d301438b37722d564cba7
Matthew-Tangk <matthew.tangkilissan@gmail.com>
2023-06-01 15:27:24 +0200

    Big upload update
    Upload page is now functioning

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-console.log("This is a testrun.");
+console.log("Welcome to FastPass");
 const express = require("express");
 const app = express();
+const multer = require("multer");
+
+const upload = multer({ dest: "uploads/" });
 
 app
   .use(express.static("static"))
@@ -25,7 +28,18 @@ app
     res.render("upload.ejs");
   })
 
+  .get("/finish", (req, res) => {
+    res.render("thankU.ejs");
+  })
+
   .listen(420);
+
+app.post("/upload", upload.array("files", 3), function (req, res) {
+  const files = req.files;
+  console.log(files);
+
+  res.send("Files uploaded successfully!");
+});
 
 function onhome(req, res) {
   res.send("<h1>This is a testrun.</h1>");
